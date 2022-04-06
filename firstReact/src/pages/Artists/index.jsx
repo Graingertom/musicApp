65f94157ce799e7d1f71866c165cc7325393ec3c
Tom Grainger <Tom.Luke.Grainger@gmail.com>
2022-04-06 16:37:01 +0100

    adding images for carousel

--- firstReact/src/pages/Artists/index.jsx
+++ firstReact/src/pages/Artists/index.jsx
@@ -4,8 +4,8 @@ import './style.css';
 
 export function Artists() {
     const [artists, setArtists] = useState([
-        {id: 101, artist: "Bon Iver", musicType: "Folk", introduction: "Bon Iver is the name singer/songwriter  chose for his solo project when he retreated to a cabin in Wisconsin during 2006.", album: [{title: "For Emma, Forever Ago", cover: ''}, {title: "Album 2", cover: ''}], img: 'https://drive.google.com/uc?export=view&id=1Q3loxL21zBhAqynHXrTVOSocn3S-yP2b'},
-        {id: 102, artist: "Frank Ocean", musicType: "R&B", introduction: "Visionary American recording artist Frank Ocean is one of the most celebrated and critically acclaimed artists of this era.", album: [{title: "Channel Orange", cover: ''}, {title: "Album 2", cover: ''}], img: 'https://drive.google.com/uc?export=view&id=1qkVpXfFJWehwpa9whZJvdCDbSnXAbAg1'}
+        {id: 101, artist: "Bon Iver", musicType: "Folk", introduction: "Bon Iver is the name singer/songwriter  chose for his solo project when he retreated to a cabin in Wisconsin during 2006.", album: [{title: "For Emma, Forever Ago", cover: 'https://upload.wikimedia.org/wikipedia/en/e/e0/Bon_iver_album_cover.jpg'}, {title: "Album 2", cover: ''}], img: 'https://drive.google.com/uc?export=view&id=1Q3loxL21zBhAqynHXrTVOSocn3S-yP2b'},
+        {id: 102, artist: "Frank Ocean", musicType: "R&B", introduction: "Visionary American recording artist Frank Ocean is one of the most celebrated and critically acclaimed artists of this era.", album: [{title: "Channel Orange", cover: 'https://upload.wikimedia.org/wikipedia/en/2/28/Channel_ORANGE.jpg'}, {title: "Album 2", cover: ''}], img: 'https://drive.google.com/uc?export=view&id=1qkVpXfFJWehwpa9whZJvdCDbSnXAbAg1'}
         ])
 
         const [ chosenArtist, setChosenArtist ] = useState();
