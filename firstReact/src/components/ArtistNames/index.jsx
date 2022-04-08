7f0a6be3a8df40459ad4ff1d875b5178708ccae2
Tom Grainger <Tom.Luke.Grainger@gmail.com>
2022-04-08 14:56:45 +0100

    adding routes for url to artists

--- firstReact/src/components/ArtistNames/index.jsx
+++ firstReact/src/components/ArtistNames/index.jsx
@@ -1,12 +1,16 @@
 import React, { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 
 export const ArtistNames = ({ handleArtistSelect, artists }) => {
+    const goTo = useNavigate();
+
     return (
         <ul>
             {
                 artists.map(art => (
-                    <li key={art.id} onClick={() =>
-                    handleArtistSelect(art.id)}>
+                    <li key={art.id} onClick={() =>{
+                    goTo(`${art.artist}`)
+                    handleArtistSelect(art.id)}}>
                         <strong 
                         role="heading"
                         aria-label="artistName">{art.artist}
